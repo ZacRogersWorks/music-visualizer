df9ccb31866151a49b133f69998170b63142dd1e
zacrogersworks <hello@zacrogers.works>
2022-08-16 17:47:21 -0700

    Adjustments for mobile browsers

--- components/Play.js
+++ components/Play.js
@@ -6,40 +6,47 @@ const Play = ({ playAudio }) => {
   return (
     <Html prepend center>
       <AnimatePresence>
-      <div
-        className={
-          "bg-mi font-bold w-full h-full flex justify-center items-center"
-        }
-        exit={{opacity: 0}}
-        transition={{duration: 1, ease: "easeOut"}}
-      >
-        <motion.button
+        <div
           className={
-            "text-sky text-4xl font-light italic flex flex-col items-center"
+            "bg-mi font-bold w-full h-full flex flex-col justify-center items-center"
           }
-          onClick={() => playAudio()}
-          initial={{opacity: 0}}
-          animate={{opacity: 1}}
-          transition={{duration: .7, ease: "easeOut"}}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 1, ease: "easeOut" }}
         >
-          <svg
-          className="mb-6 hover:scale-125 transition-transform duration-300"
-            xmlns="http://www.w3.org/2000/svg"
-            width="25.602"
-            height="32"
-            viewBox="0 0 25.602 32"
+          <motion.button
+            className={
+              "text-sky text-4xl font-light italic flex flex-col items-center"
+            }
+            onClick={() => playAudio()}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.7, ease: "easeOut" }}
           >
-            <path
-              id="Icon_ionic-ios-play"
-              data-name="Icon ionic-ios-play"
-              d="M9,7.579V37.919a.781.781,0,0,0,1.17.72l24.049-15.17a.859.859,0,0,0,0-1.43L10.17,6.869A.774.774,0,0,0,9,7.579Z"
-              transform="translate(-9 -6.751)"
-              fill="#B28686" start
-            />
-          </svg>
-          PLAY
-        </motion.button>
-      </div>
+            <svg
+              className="mb-6 hover:scale-125 transition-transform duration-300"
+              xmlns="http://www.w3.org/2000/svg"
+              width="25.602"
+              height="32"
+              viewBox="0 0 25.602 32"
+            >
+              <path
+                id="Icon_ionic-ios-play"
+                data-name="Icon ionic-ios-play"
+                d="M9,7.579V37.919a.781.781,0,0,0,1.17.72l24.049-15.17a.859.859,0,0,0,0-1.43L10.17,6.869A.774.774,0,0,0,9,7.579Z"
+                transform="translate(-9 -6.751)"
+                fill="#B28686"
+                start
+              />
+            </svg>
+            PLAY
+          </motion.button>
+        </div>
+        <div className="w-[66vw] md:w-[33vw] flex">
+          <p className="mt-12 text-center">
+            <span className="uppercase text-clay mr-2">Note:</span>To hear audio
+            on iOS devices, your mute switch must be turned off.
+          </p>
+        </div>
       </AnimatePresence>
     </Html>
   );
